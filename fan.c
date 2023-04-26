fd289e9de1cd7385c1fafd4f27d50948202f7164
LAKIpoe01 <129854747+LAKIpoe01@users.noreply.github.com>
2023-04-26 17:29:12 +0300

    Add files via upload

---
+++ fan.c
@@ -0,0 +1,96 @@
+#include <Servo.h>
+#include <EEPROM.h>
+
+#define DEBUG
+
+#define INPUT_LIGHT A0
+#define INPUT_DIAL A1
+#define INPUT_BUTTON 2
+#define OUTPUT_SERVO 9
+
+#define ANALOG_MAX 1023
+#define FREQ 20
+#define TICK_LENGTH 50 // 1000/FREQ
+#define DIAL_THRESHOLD 50
+#define DIAL_THRESHOLD_INCR 51 // DIAL_THRESHOLD + 1
+#define SERVO_MIN 93
+#define SERVO_MAX 180
+#define EEPROM_ADDRESS 0
+
+Servo servo;
+
+char tick = 0;
+bool last_button = false;
+int servo_old = 0;
+
+int threshold = 40;
+
+void setup() {
+  pinMode(INPUT_LIGHT, INPUT);
+  pinMode(INPUT_DIAL, INPUT);
+  pinMode(INPUT_BUTTON, INPUT);
+
+  servo.attach(OUTPUT_SERVO);
+  
+  // Read threshold from eeprom.
+  threshold = ((int) EEMPROM.read(EEPROM_ADDRESS) << 8) + EEPROM.read(EEPROM_ADDRESS + 1);
+
+#ifdef DEBUG
+  Serial.begin(9600);
+  Serial.println("Start");
+#endif
+}
+
+void loop() {
+  const int light = analogRead(INPUT_LIGHT);
+  const int dial = analogRead(INPUT_DIAL);
+  const bool button = digitalRead(INPUT_BUTTON) == HIGH;
+
+  // When the button is pressed recalculate the target up, down and threshold.
+  if (button && !last_button) {
+    const int new_threshold = 
+      erf(((double) light - (double) ANALOG_MAX) / (double) ANALOG_MAX) 
+      * (double) ANALOG_MAX + 862.0d;
+    
+    if (new_threshold != threshold) {
+      threshold = new_threshold;
+      
+      // Write threshold to eeprom.
+      EEPROM.write(EEPROM_ADDRESS, threshold >> 8);
+      EEPROM.write(EEPROM_ADDRESS + 1, threshold & 0xFF);
+	}
+  }
+
+  // This block is executed once a second (ish).
+  if (tick > FREQ) {
+    // Dial can be also used as a manual off switch, when it is turned close to zero.
+    const int servo_new = (dial > DIAL_THRESHOLD && light > threshold)
+      ? map(dial, DIAL_THRESHOLD_INCR, ANALOG_MAX, SERVO_MIN, SERVO_MAX)
+      : SERVO_MIN
+    ;
+
+    // Don't change the servo value, if the speed is already desired.
+    if (servo_old != servo_new) {
+      servo_old = servo_new;
+      servo.write(servo_new);
+    }
+
+#ifdef DEBUG
+    Serial.print("Threshold: ");
+    Serial.print(threshold);
+    Serial.print(" Dial: ");
+    Serial.print(dial);
+    Serial.print(" Light: ");
+    Serial.print(light);
+    Serial.print(" Servo: ");
+    Serial.print(servo_old);
+    Serial.println();
+#endif
+
+    tick = 0;
+  }
+
+  last_button = button;
+  tick++;
+  delay(TICK_LENGTH);
+}
